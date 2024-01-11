3fa3a1a208127d8e636845dc0f50c0036d41dada
grainrigi <naonaokiryu2@gmail.com>
2024-01-11 17:07:18 +0900

    fix remote url

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,66 @@
 'use strict';
 
-var gulp = require( 'gulp' );
-var download = require( 'gulp-download' );
-var decompress = require( 'gulp-decompress' );
-var convertEncoding = require( 'gulp-convert-encoding' );
-var chmod = require( 'gulp-chmod' );
-var rename = require( 'gulp-rename' );
-var postal2json = require( './lib/postal2json.js' );
-var jigyosyo2json = require( './lib/jigyosyo2json.js' );
-var v1 = require( './lib/v1.js' );
-var v2 = require( './lib/v2.js' );
+var gulp = require('gulp');
+var download = require('gulp-download');
+var decompress = require('gulp-decompress');
+var convertEncoding = require('gulp-convert-encoding');
+var chmod = require('gulp-chmod');
+var rename = require('gulp-rename');
+var postal2json = require('./lib/postal2json.js');
+var jigyosyo2json = require('./lib/jigyosyo2json.js');
+var v1 = require('./lib/v1.js');
+var v2 = require('./lib/v2.js');
 
-gulp.task( 'download', function () {
+gulp.task('download', function () {
   var urls = [
-    'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip',
+    'https://www.post.japanpost.jp/zipcode/dl/roman/KEN_ALL_ROME.zip',
     'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip',
     'http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip'
   ];
-  return download( urls )
-    .pipe( decompress() )
-    .pipe( convertEncoding( { from: "shift_jis", to: "utf-8" } ) )
-    .pipe( chmod( 644 ) )
-    .pipe( rename(path => {
+  return download(urls)
+    .pipe(decompress())
+    .pipe(convertEncoding({ from: "shift_jis", to: "utf-8" }))
+    .pipe(chmod(644))
+    .pipe(rename(path => {
       path.basename = path.basename.toUpperCase();
       path.extname = path.extname.toUpperCase();
     }))
-    .pipe( gulp.dest( 'api' ) );
-} );
+    .pipe(gulp.dest('api'));
+});
 
 /**
  * Create an API of the postal code.
  */
-gulp.task( 'v1', function () {
-  return gulp.src( 'api/KEN_ALL_ROME.CSV' )
-    .pipe( postal2json() )
-    .pipe( v1() )
-    .pipe( chmod( 644 ) )
-    .pipe( gulp.dest( 'api/v1' ) );
-} );
+gulp.task('v1', function () {
+  return gulp.src('api/KEN_ALL_ROME.CSV')
+    .pipe(postal2json())
+    .pipe(v1())
+    .pipe(chmod(644))
+    .pipe(gulp.dest('api/v1'));
+});
 
 /**
  * Create an API of the Jigyosyo postal code.
  */
-gulp.task( 'v1-jigyosyo', function () {
-  return gulp.src( 'api/JIGYOSYO.CSV' )
-    .pipe( jigyosyo2json() )
-    .pipe( v1() )
-    .pipe( chmod( 644 ) )
-    .pipe( gulp.dest( 'api/v1' ) );
-} );
+gulp.task('v1-jigyosyo', function () {
+  return gulp.src('api/JIGYOSYO.CSV')
+    .pipe(jigyosyo2json())
+    .pipe(v1())
+    .pipe(chmod(644))
+    .pipe(gulp.dest('api/v1'));
+});
 
 /**
  * Create an v2 API
  */
-gulp.task( 'v2', function () {
-  return gulp.src( 'gulpfile.js' ) // dummy input
-    .pipe( v2() )
-    .pipe( chmod( 644 ) )
-    .pipe( gulp.dest( 'api/v2' ) );
-} );
+gulp.task('v2', function () {
+  return gulp.src('gulpfile.js') // dummy input
+    .pipe(v2())
+    .pipe(chmod(644))
+    .pipe(gulp.dest('api/v2'));
+});
 
 exports.default = gulp.series(
   'download',
-  gulp.parallel( 'v1', 'v1-jigyosyo', 'v2' ),
+  gulp.parallel('v1', 'v1-jigyosyo', 'v2'),
 );
